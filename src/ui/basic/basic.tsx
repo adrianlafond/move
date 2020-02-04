2aee53d816575382b31f04bc11aa019d2eaedaeb
Adrian Lafond <mail@adrianlafond.com>
2020-02-03 22:23:21 -0500

    enable preact app

--- src/ui/basic/basic.tsx
+++ src/ui/basic/basic.tsx
@@ -1,14 +1,53 @@
 import { h, FunctionalComponent } from 'preact';
-import { MoveTimer } from '../../move-timer';
+import { useState, useEffect } from 'preact/hooks';
+import { UiProps } from '../types';
+import { TimeDisplay } from '../../time-display';
 
 import './basic.css';
 
-export const Basic: FunctionalComponent<{ timer: MoveTimer }> = ({ timer }) => {
+export const Basic: FunctionalComponent<UiProps> = ({ timer }) => {
+  const [time, setTime] = useState(
+    `${TimeDisplay.toMinutesSecondsWithZeroes(timer.time)}`,
+  );
+  const [actionLabel, setActionLabel] = useState('Start');
+
+  function onTime(milliseconds: number) {
+    const isComplete = milliseconds === 0;
+    setTime(
+      isComplete
+        ? 'Move!'
+        : `${TimeDisplay.toMinutesSecondsWithZeroes(milliseconds)}`,
+    );
+    setActionLabel(isComplete ? 'Start' : 'Pause');
+  }
+
+  function onAction() {
+    if(timer.isPlaying) {
+      timer.pause();
+      setActionLabel('Start');
+    } else {
+      timer.play();
+      setActionLabel('Pause');
+    }
+  }
+
+  function onReset() {
+    timer.stop();
+    setActionLabel('Start');
+  }
+
+  useEffect(() => {
+    timer.addTimeListener(onTime);
+    return () => {
+      timer.removeTimeListener(onTime);
+    };
+  });
+
   return (
     <div className="basic">
-      <h1 className="basic__time">00:00</h1>
-      <button className="basic__button-action">Start</button>
-      <button className="basic__button-reset">Reset</button>
+      <h1 className="basic__time">{time}</h1>
+      <button onClick={onAction} className="basic__button-action">{actionLabel}</button>
+      <button onClick={onReset} className="basic__button-reset">Reset</button>
     </div>
   );
 };
